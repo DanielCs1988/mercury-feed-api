0cedc8c1befc652a0f64cf52002108d9ecff3404
Emperor <daniel.cs20@gmail.com>
2018-07-09 11:42:53 +0200

    trying CORS solution

--- src/index.ts
+++ src/index.ts
@@ -28,4 +28,11 @@ const server = new GraphQLServer({
 
 server.express.post(server.options.endpoint!, validateJwt);
 server.express.post(server.options.endpoint!, (req, res, done) => getCurrentUserId(req, res, done, prisma));
+server.express.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
+    next();
+});
 server.start(() => console.log('Server is running ...'));
